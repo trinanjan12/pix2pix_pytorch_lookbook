da6729aaff9456a173ce3e9c7256fbd12e099526
trinanjan <trinanjan12@gmail.com>
2020-03-31 09:53:32 +0530

    added dataset download link

--- download_data.sh
+++ download_data.sh
@@ -1,2 +1,6 @@
-wget https://drive.google.com/uc?export=download&confirm=N7nB&id=0By_p0y157GxQU1dCRUU4SFNqaTQ
-tar -xvf lookbook.tar
+#!/bin/bash
+fileid="0By_p0y157GxQU1dCRUU4SFNqaTQ"
+filename="lookbook.tar"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+tar -xvf $filename
